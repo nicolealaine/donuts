a73c664c57c8052c81e8b69d19ef7ea0b051bebd
Nicole Crayne <n@crayne.net>
2023-11-13 09:59:13 -0800

    Initial commit

---
+++ assets/js/nav.js
@@ -0,0 +1,36 @@
+
+
+    function myFunction(x) {
+        const menu = document.querySelector('.menu');
+
+        x.classList.toggle("change");
+        if (menu.classList.contains('showMenu')) {
+            menu.classList.remove('showMenu')
+        } else {
+            menu.classList.add('showMenu')
+        }
+    }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+        const menu = document.querySelector('.menu');
+        const menuItems = document.querySelectorAll('.menuItem');
+
+            function toggleMenu() {
+                if (menu.classList.contains('showMenu')) {
+                menu.classList.remove('showMenu')
+            } else {
+                menu.classList.add('showMenu')
+            }
+        }
+
+    menuItems.forEach(
+        function (menuItem) {
+            menuItem.addEventListener("click", toggleMenu);
+        }
+    )
+
+  
+     
+})
+
+
